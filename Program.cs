398516970ef344b819163820dbb3eca88a12804d
damianoneill <damian.oneill@gmail.com>
2019-08-13 18:41:49 +0100

    menu with options

--- Program.cs
+++ Program.cs
@@ -5,22 +5,43 @@ namespace mx
 {
     class Program
     {
+        static public int DisplayMenu()
+        {
+            Console.WriteLine("Motocross Querier");
+            Console.WriteLine();
+            Console.WriteLine("1. Show data from WICK-338");
+            Console.WriteLine("2. Show data from HANGTOWN");
+            Console.WriteLine("0. Exit");
+            var result = Console.ReadLine();
+            return Convert.ToInt32(result);
+        }
+
         static void Main(string[] args)
         {
             var client = new WebClient();
-
-            string url = "https://api.promotocrossapi.com/laptimes/18/WICK-338";
-            var response = client.DownloadString(url);
-
-            var mx = Welcome.FromJson(response);
-
-            Console.WriteLine("There was " + mx.Data.Length + " Races in 2018 at the WICK-338 track");
-
-            url = "https://api.promotocrossapi.com/laptimes/17/HANGTOWN";
-            response = client.DownloadString(url);
-            mx = Welcome.FromJson(response);
-
-            Console.WriteLine("There was " + mx.Data.Length + " Races in 2017 at the HANGTOWN track");
+            int userInput = 0;
+            string url = "", track = "", response;
+            Welcome mx;
+            do
+            {
+                userInput = DisplayMenu();
+                if (userInput == 1)
+                {
+                    url = "https://api.promotocrossapi.com/laptimes/18/WICK-338";
+                    track = "WICK-338";
+                }
+                if (userInput == 2)
+                {
+                    url = "https://api.promotocrossapi.com/laptimes/18/HANGTOWN";
+                    track = "HANGTOWN";
+                }
+                if (userInput != 0)
+                {
+                    response = client.DownloadString(url);
+                    mx = Welcome.FromJson(response);
+                    Console.WriteLine("There was " + mx.Data.Length + " Races in 2018 at the " + track + " track");
+                }
+            } while (userInput != 0);
         }
     }
 }
