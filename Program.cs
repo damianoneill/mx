8bac1db7b35b10abab21964976990ce295ee4488
damianoneill <damian.oneill@gmail.com>
2019-08-13 18:01:47 +0100

    multi-requests

--- Program.cs
+++ Program.cs
@@ -8,12 +8,19 @@ namespace mx
         static void Main(string[] args)
         {
             var client = new WebClient();
+
             string url = "https://api.promotocrossapi.com/laptimes/18/WICK-338";
             var response = client.DownloadString(url);
 
             var mx = Welcome.FromJson(response);
 
             Console.WriteLine("There was " + mx.Data.Length + " Races in 2018 at the WICK-338 track");
+
+            url = "https://api.promotocrossapi.com/laptimes/17/HANGTOWN";
+            response = client.DownloadString(url);
+            mx = Welcome.FromJson(response);
+
+            Console.WriteLine("There was " + mx.Data.Length + " Races in 2017 at the HANGTOWN track");
         }
     }
 }
